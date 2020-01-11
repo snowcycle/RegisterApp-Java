5e292854ca04b46d1cec0339f0e66389145b9b33
Reid Phillips <xorlogic@gmail.com>
2020-01-11 11:11:59 -0600

    Updates to the product details route controller for sprint 2 functionality, incomplete.

--- src/main/java/edu/uark/registerapp/controllers/ProductRestController.java
+++ src/main/java/edu/uark/registerapp/controllers/ProductRestController.java
@@ -2,6 +2,9 @@ package edu.uark.registerapp.controllers;
 
 import java.util.UUID;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,16 +16,29 @@ import org.springframework.web.bind.annotation.RestController;
 import edu.uark.registerapp.commands.products.ProductCreateCommand;
 import edu.uark.registerapp.commands.products.ProductDeleteCommand;
 import edu.uark.registerapp.commands.products.ProductUpdateCommand;
+import edu.uark.registerapp.controllers.enums.ViewNames;
 import edu.uark.registerapp.models.api.ApiResponse;
 import edu.uark.registerapp.models.api.Product;
 
 @RestController
 @RequestMapping(value = "/api/product")
-public class ProductRestController {
+public class ProductRestController extends BaseRestController {
 	@RequestMapping(value = "/", method = RequestMethod.POST)
 	public @ResponseBody ApiResponse createProduct(
-		@RequestBody final Product product
+		@RequestBody final Product product,
+		final HttpServletRequest request,
+		final HttpServletResponse response
 	) {
+
+		final ApiResponse elevatedUserResponse =
+			this.redirectUserNotElevated(
+				request,
+				response,
+				ViewNames.PRODUCT_LISTING.getRoute());
+
+		if (!elevatedUserResponse.getRedirectUrl().equals(StringUtils.EMPTY)) {
+			return elevatedUserResponse;
+		}
 
 		return this.productCreateCommand
 			.setApiProduct(product)
@@ -35,6 +51,8 @@ public class ProductRestController {
 		@RequestBody final Product product
 	) {
 
+		// TODO: Verify that the user associated with the current session is elevated
+
 		return this.productUpdateCommand
 			.setProductId(productId)
 			.setApiProduct(product)
@@ -45,6 +63,8 @@ public class ProductRestController {
 	public @ResponseBody ApiResponse deleteProduct(
 		@PathVariable final UUID productId
 	) {
+
+		// TODO: Verify that the user associated with the current session is elevated
 
 		this.productDeleteCommand
 			.setProductId(productId)
