ddb69d1aac30d0ac2ba869b832903313e75c6e43
Jacob Dedman <jacob.dedman@gmail.com>
2020-10-08 11:23:02 -0500

    Task 5 commit

---
+++ src/main/java/edu/uark/registerapp/commands/employees/ActiveEmployeeExistsQuery.java
@@ -0,0 +1,20 @@
+package edu.uark.registerapp.commands.employees;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import edu.uark.registerapp.commands.VoidCommandInterface;
+import edu.uark.registerapp.commands.exceptions.NotFoundException;
+import edu.uark.registerapp.models.repositories.EmployeeRepository;
+
+@Service
+public class ActiveEmployeeExistsQuery implements VoidCommandInterface {
+	@Override
+	public void execute() {
+		if (!this.employeeRepository.existsByIsActive(true)) {
+			throw new NotFoundException("Employee");
+		}
+	}
+	@Autowired
+	private EmployeeRepository employeeRepository;
+}
