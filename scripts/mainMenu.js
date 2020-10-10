f53f4182d51c0cb4db5cf0ea52cf1968d2feab08
narmineba <narmineba@gmail.com>
2020-10-10 14:47:40 -0500

    Task6

---
+++ scripts/mainMenu.js
@@ -0,0 +1,16 @@
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById("start_TransactionButton").addEventListener("click", function() {
+        displayError("Functionality has not yet been implemented");
+    });  
+    document.getElementById("view_ProductsButton").addEventListener("click", function() {
+        location.assign("/productListing")});
+    document.getElementById("Create_EmployeeButton").addEventListener("click", function() {
+        location.assign("/employeeDetail")});
+    document.getElementById("Sales_ReportButton").addEventListener("click", function() {
+        displayError("Functionality has not yet been implemented")});
+    document.getElementById("Cashier_ReportButton").addEventListener("click", function() {
+        displayError("Functionality has not yet been implemented")});
+    document.getElementById("signOutImage").addEventListener("click", function() {
+            location.assign("/")});
+    }); 
