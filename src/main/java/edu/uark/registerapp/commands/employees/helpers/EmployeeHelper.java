ffae193d6328a1ddc2ad08fe7dd9db79d33b89a2
Max <jedicobra@gmail.com>
2021-02-11 16:52:04 -0600

    implemented employee password hashing

--- src/main/java/edu/uark/registerapp/commands/employees/helpers/EmployeeHelper.java
+++ src/main/java/edu/uark/registerapp/commands/employees/helpers/EmployeeHelper.java
@@ -2,6 +2,10 @@ package edu.uark.registerapp.commands.employees.helpers;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class EmployeeHelper {
     public static String padEmployeeId(final int employeeId) {
         final String employeeIdAsString = Integer.toString(employeeId);
@@ -15,8 +19,17 @@ public class EmployeeHelper {
     }
 
     public static byte[] hashPassword(final String password) {
-        // TODO: Hash the password using a MessageDigest. An example can be found at http://tutorials.jenkov.com/java-cryptography/messagedigest.html
-        return new byte[0];
+        // This exception should maybe not be handled within the method
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            // Convert password string to byte array
+            byte [] passwordBytes = password.getBytes(StandardCharsets.UTF_8);
+            return messageDigest.digest(passwordBytes);
+        } catch(NoSuchAlgorithmException e){
+            // I don't know how to handle errors but this probably won't happen anyway
+            System.out.println("Sorry, SHA-256 doesn't exist actually");
+            return new byte[0];
+        }
     }
 
     private static final int EMPLOYEE_ID_MAXIMUM_LENGTH = 5;
