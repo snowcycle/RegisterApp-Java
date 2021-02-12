0fbae3b6f3e4b4e84084d4053852db806309e5d2
moyeneka <60825726+moyeneka@users.noreply.github.com>
2021-02-12 14:06:53 -0600

    routing class

--- src/main/java/edu/uark/registerapp/controllers/MainMenuRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/MainMenuRouteController.java
@@ -1,5 +1,22 @@
 package edu.uark.registerapp.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+
+// methods
 public class MainMenuRouteController {
+	// @GetMapping("/mainMenu")
+	// @ResponseBody
+	// public String getMainMenu()// @RequestParam Map<String, String> object)
+	// {
+
+	// }
+	// methods
 
 }
+
+// Properties
