7be099d2133e7d2c8e7b18271db310d257eb8937
moyeneka <60825726+moyeneka@users.noreply.github.com>
2021-02-15 21:24:59 -0600

    Update MainMenuRouteController.java

--- src/main/java/edu/uark/registerapp/controllers/MainMenuRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/MainMenuRouteController.java
@@ -1,22 +1,59 @@
 package edu.uark.registerapp.controllers;
 
+import java.util.Map;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+import edu.uark.registerapp.controllers.enums.ViewModelNames;
+import edu.uark.registerapp.controllers.enums.ViewNames;
+import edu.uark.registerapp.models.entities.ActiveUserEntity;
 
-// methods
-public class MainMenuRouteController {
-	// @GetMapping("/mainMenu")
-	// @ResponseBody
-	// public String getMainMenu()// @RequestParam Map<String, String> object)
-	// {
-	/// nhnhn
-	// }
-	// methods
+// @Controller
+// // methods
+// public class MainMenuRouteController {
+// 	// @GetMapping("/mainMenu")
+// 	// @ResponseBody
+// 	// public String getMainMenu()// @RequestParam Map<String, String> object)
+// 	// {
+// 	/// nhnhn
+// 	// }
+// 	// methods
 
-}
+// }
 
 // Properties
+
+@RequestMapping(value = "/mainMenu")
+public class MainMenuRouteController extends BaseRouteController {
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView start(
+		@RequestParam final Map<String, String> queryParameters,
+		final HttpServletRequest request
+	) {
+
+		final Optional<ActiveUserEntity> activeUserEntity =
+			this.getCurrentUser(request);
+		if (!activeUserEntity.isPresent()) {
+			return this.buildInvalidSessionResponse();
+		}
+		
+		ModelAndView modelAndView =
+			this.setErrorMessageFromQueryString(
+				new ModelAndView(ViewNames.MAIN_MENU.getViewName()),
+				queryParameters);
+		// TODO: Examine the ActiveUser classification if you want this information
+		modelAndView.addObject(
+			ViewModelNames.IS_ELEVATED_USER.getValue(),
+			true);
+		
+		return modelAndView;
+	}
+}
