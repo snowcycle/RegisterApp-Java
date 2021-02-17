5bb9e8d0d15879f9caf2ea9b11da9b157e8eaab2
Chase <chasegarrett@tutanota.com>
2021-02-16 20:02:06 -0600

    add method to query database

--- src/main/java/edu/uark/registerapp/commands/employees/ActiveEmployeeExistsQuery.java
+++ src/main/java/edu/uark/registerapp/commands/employees/ActiveEmployeeExistsQuery.java
@@ -1,6 +1,11 @@
 package edu.uark.registerapp.commands.employees;
 
 import org.apache.commons.lang3.StringUtils;
+import edu.uark.registerapp.models.repositories.EmployeeRepository;
+import edu.uark.registerapp.commands.exceptions.NotFoundException;
 
-public class ActiveEmployeeExistsQuery{
+public class ActiveEmployeeExistsQuery implements EmployeeRepository{
+    public boolean existsByIsActive(boolean isActive){
+        return isActive;
+    }
 }
