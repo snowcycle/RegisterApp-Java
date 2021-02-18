02e26659637aff76df689baea92adc3408417019
Max <jedicobra@gmail.com>
2021-02-18 13:09:31 -0600

    added sign in page and also to ViewNames?

---
+++ src/main/resources/static/scripts/signIn.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+	// TODO: Anything you want to do when the page is loaded?
+});
+
+function validateForm() {
+	// TODO: Validate the user input
+	return true;
+}
