4a8a40f615483bdc45cc0086967155b3077a6ce6
IsogaiYugo <41026474+IsogaiYugo@users.noreply.github.com>
2021-02-19 06:23:09 +0900

    Update signIn.js
    create functions of validate whether EmployeeID & Password are blank and whether EmployeeID is index

--- src/main/resources/static/scripts/signIn.js
+++ src/main/resources/static/scripts/signIn.js
@@ -4,5 +4,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 function validateForm() {
 	// TODO: Validate the user input
-	return true;
+	//get value of Employee ID and Password
+	var employeeId = document.getElementById("employeeId").value;
+	var password = document.getElementById("password").value;
+
+	if (employeeId != '' && password != ''){
+		if (isNaN(employeeId)) {
+			document.getElementById("errorMessage").innerHTML = "You can use only index"
+		}
+		else {
+			document.getElementById("errorMessage").innerHTML = "Sign in successful!!"
+			return true;
+		}
+	}
+	else{
+		document.getElementById("errorMessage").innerHTML = "Please do not keep blank Employee ID & Password"
+		return false;
+	}
 }
