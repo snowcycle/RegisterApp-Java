14eb849659f6c4fe5b313cef41401a1578d5b606
Chase <chasegarrett@tutanota.com>
2021-02-19 16:53:38 -0600

    Complete ActiveEmployeeExistsQuery class, ready for testing

--- src/main/java/edu/uark/registerapp/commands/employees/ActiveEmployeeExistsQuery.java
+++ src/main/java/edu/uark/registerapp/commands/employees/ActiveEmployeeExistsQuery.java
@@ -4,14 +4,16 @@ import org.apache.commons.lang3.StringUtils;
 import edu.uark.registerapp.models.repositories.EmployeeRepository;
 import edu.uark.registerapp.commands.exceptions.NotFoundException;
 
+@Service
 public class ActiveEmployeeExistsQuery implements EmployeeRepository{
-    public boolean existsByIsActive(boolean isActive){
-        return isActive;
-    }
-
-    public void checkIfActive(boolean isActive){
+    @Override
+    public void checkIfActive(){
+        boolean isActive = existsByIsActive(isActive);
         if(!isActive){
-            throw new NotFoundException();
+            throw new NotFoundException("Employee");
         }
     }
+
+    @Autowire
+    private EmployeeRepository employeeRepository;
 }
