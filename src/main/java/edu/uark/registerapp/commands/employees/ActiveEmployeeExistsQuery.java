6cccec9be64fa75f20d1c06ab22ae1898c7c9784
Chase <chasegarrett@tutanota.com>
2021-02-20 01:16:32 -0600

    Cleaned code and imported dependencies

--- src/main/java/edu/uark/registerapp/commands/employees/ActiveEmployeeExistsQuery.java
+++ src/main/java/edu/uark/registerapp/commands/employees/ActiveEmployeeExistsQuery.java
@@ -3,13 +3,14 @@ package edu.uark.registerapp.commands.employees;
 import org.apache.commons.lang3.StringUtils;
 import edu.uark.registerapp.models.repositories.EmployeeRepository;
 import edu.uark.registerapp.commands.exceptions.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class ActiveEmployeeExistsQuery implements EmployeeRepository{
     @Override
     public void checkIfActive(){
-        boolean isActive = employeeRepository.existsByIsActive(isActive);
-        if(!isActive){
+        if(!this.employeeRepository.existsByIsActive(true)){
             throw new NotFoundException("Employee");
         }
     }
