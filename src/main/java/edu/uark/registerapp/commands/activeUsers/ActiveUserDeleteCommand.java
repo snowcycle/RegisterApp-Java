88f56e0e505a981ac494451722ee9b76b1f750af
Chase <chasegarrett@tutanota.com>
2021-02-27 13:10:01 -0600

    Update to dpendencies

--- src/main/java/edu/uark/registerapp/commands/activeUsers/ActiveUserDeleteCommand.java
+++ src/main/java/edu/uark/registerapp/commands/activeUsers/ActiveUserDeleteCommand.java
@@ -1,6 +1,14 @@
 package edu.uark.registerapp.commands.activeUsers;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import edu.uark.registerapp.commands.ResultCommandInterface;
+import edu.uark.registerapp.commands.exceptions.UnauthorizedException;
+import edu.uark.registerapp.nodels.entities.ActiveUserEntity;
+import edu.uark.registerapp.models.repositories.ActiveUserRepository;
+
+@Service
 public class ActiveUserDeleteCommand{
 }
