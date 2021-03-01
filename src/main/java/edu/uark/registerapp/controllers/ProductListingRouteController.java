51ce71395121cea8e30467a3e1c192055d600a71
moyeneka <60825726+moyeneka@users.noreply.github.com>
2021-03-01 13:35:34 -0600

    Update ProductListingRouteController.java

--- src/main/java/edu/uark/registerapp/controllers/ProductListingRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/ProductListingRouteController.java
@@ -12,7 +12,7 @@ import edu.uark.registerapp.controllers.enums.ViewNames;
 import edu.uark.registerapp.models.api.Product;
 
 @Controller
-@RequestMapping(value = "/productListing")
+@RequestMapping(value = "/")
 public class ProductListingRouteController {
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView showProductListing() {
