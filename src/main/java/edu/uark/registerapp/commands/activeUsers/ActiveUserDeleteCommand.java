f3ba2f7eed3a71e8ac4704813ca927eaef103b35
Chase <chasegarrett@tutanota.com>
2021-03-01 12:39:16 -0600

    update to dependencies and begun to build out the class

--- src/main/java/edu/uark/registerapp/commands/activeUsers/ActiveUserDeleteCommand.java
+++ src/main/java/edu/uark/registerapp/commands/activeUsers/ActiveUserDeleteCommand.java
@@ -1,8 +1,12 @@
 package edu.uark.registerapp.commands.activeUsers;
 
+import java.util.Optional;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.pringframework.beans.factory.annotation.Transactional;
+
 
 import edu.uark.registerapp.commands.ResultCommandInterface;
 import edu.uark.registerapp.commands.exceptions.UnauthorizedException;
@@ -11,4 +15,29 @@ import edu.uark.registerapp.models.repositories.ActiveUserRepository;
 
 @Service
 public class ActiveUserDeleteCommand{
+    @Override
+    public ActiveUserEntity execute(){
+        final Optional<ActiveUserEntity> activeUserEntity=
+            this.activeUserRepository.findBySessionKey(this.sessionKey);
+
+        if(!activeUserEntity.isPresent()){
+            throw new UauthorizedException();
+        }
+
+        return activeUserEntity.get();
+    }
+
+    private String sessionKey;
+
+    public String getSessionKey(){
+        return this.sessionKey;
+    }
+    
+    @Transactional
+    public ActiveUserDeleteCommand(){
+
+    }
+
+    @Autowired
+    private ActiveUserRepository activeUserRepository;
 }
