fe178bc508e2890f4f98c4484e001eb5f4faf45d
Jackson Bullard <jsbullar@uark.edu>
2021-03-02 13:03:23 -0600

    Final small changes before mergin with Sprint2Starter. Need to merge because some of the stuff from Sprint2Employee is needed for further sign in development.

---
+++ src/main/java/edu/uark/registerapp/commands/employees/EmployeeSignInCommand.java
@@ -0,0 +1,14 @@
+package edu.uark.registerapp.commands.employees;
+
+import edu.uark.registerapp.models.api.EmployeeSignIn;
+
+public class EmployeeSignInCommand {
+
+	private EmployeeSignIn employeeSignIn;
+	private String sessionKey;
+	
+	// TO DO: Validate employeeSignIn's employeeId and password
+
+	// TO DO: Query employee by id and verify matching passwords
+
+}
