f8a1bea094350fce9ad90d0d168eb9793da977ca
Patrick <karangwapa@gmail.com>
2021-03-02 14:12:22 -0600

    Fixed products listing button

--- src/main/resources/static/scripts/mainMenu.js
+++ src/main/resources/static/scripts/mainMenu.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   getStartTransactionActionElement().addEventListener("click", () => { displayError("Functionality has not yet been implemented."); });
-  getViewProductsActionElement().addEventListener("click", () => { window.location.assign("/productListing"); });
+  getViewProductsActionElement().addEventListener("click", () => { window.location.assign("productListing.html"); });
   getCreateEmployeeActionElement().addEventListener("click", () => { window.location.assign("/employeeDetail"); });
   getProductSalesReportActionElement().addEventListener("click", () => { displayError("Functionality has not yet been implemented."); });
   getCashierSalesReportActionElement().addEventListener("click", () => { displayError("Functionality has not yet been implemented."); });
