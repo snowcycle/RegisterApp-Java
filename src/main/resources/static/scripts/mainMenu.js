36195b55c871f763447249490f14eb5f9e513eda
Jodicd <jodibug21@gmail.com>
2021-03-03 15:48:31 -0600

    JM - The real task 7

---
+++ src/main/resources/static/scripts/mainMenu.js
@@ -0,0 +1,45 @@
+// This is the functionality for the main menu.
+document.addEventListener("DOMContentLoaded", function(event) 
+{
+	startTransaction.addEventListener("click", transactionActionElement);
+
+	viewProducts.addEventListener("click", productsActionElement);
+
+	createEmployee.addEventListener("click", employeeActionElement);
+
+	salesReport.addEventListener("click", productSalesReportActionElement);
+
+	cashiersReport.addEventListener("click", cashierSalesReportActionElement);
+
+	signOut.addEventListener("click", signOutActionElement); // signOut is the id of the sign out button
+	
+});
+
+
+function transactionActionElement() {
+	document.getElementById("errorMessage").innerHTML = "Functionality has not yet been implemented";
+}
+
+function productsActionElement() {
+	window.location.href = "\productListing.html"; // go to the product listing page
+}
+
+function employeeActionElement() {
+	window.location.href = "\employeeDetails.html"; // go to the employee detail page
+}
+
+function productSalesReportActionElement() {
+	document.getElementById("errorMessage").innerHTML = "Functionality has not yet been implemented";
+
+}
+
+function cashierSalesReportActionElement() {
+	document.getElementById("errorMessage").innerHTML = "Functionality has not yet been implemented";
+
+}
+
+function signOutActionElement() {
+
+	window.location.href = "\signIn.html"; // Go to the sign in page
+
+}
