168d0bd817386423e6fc3540f4da21f21c94e55f
pingyhead <42194751+pingyhead@users.noreply.github.com>
2021-03-02 21:01:32 -0600

    Update SignInRestController.java

--- src/main/java/edu/uark/registerapp/controllers/SignInRestController.java
+++ src/main/java/edu/uark/registerapp/controllers/SignInRestController.java
@@ -2,11 +2,13 @@ package edu.uark.registerapp.controllers;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.uark.registerapp.commands.activeUsers.ActiveUserDeleteCommand;
 import edu.uark.registerapp.controllers.enums.ViewNames;
 import edu.uark.registerapp.models.api.ApiResponse;
 
@@ -18,9 +20,15 @@ public class SignInRestController extends BaseRestController {
 		final HttpServletRequest request
 	) {
 
-		// TODO: Sign out the user associated with request.getSession().getId()
+		this.activeUserDeleteCommand
+			.setSessionKey(request.getSession().getId())
+			.execute();
 
 		return (new ApiResponse())
 			.setRedirectUrl(ViewNames.SIGN_IN.getRoute());
 	}
+
+	// Properties
+	@Autowired
+	private ActiveUserDeleteCommand activeUserDeleteCommand;
 }
