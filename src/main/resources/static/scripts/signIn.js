4aa8ea7cf7171683af3c2be9cf40496172509352
Jackson Bullard <jsbullar@uark.edu>
2021-03-03 17:47:58 -0600

    Task 3 COMPLETE!

--- src/main/resources/static/scripts/signIn.js
+++ src/main/resources/static/scripts/signIn.js
@@ -1,9 +1,37 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function(event)
+{
 	// TODO: Anything you want to do when the page is loaded?
 	// TODO: Check for any defined employees - If none, redirect to employee detail
 });
 
-function validateForm() {
-	// TODO: Validate the user input
+function validateForm()
+{
+	// Validate employeeId
+	let employeeId = document.forms["signIn"]["employeeId"].value;
+
+	if (employeeId.length == 0)
+	{
+		alert("Please enter your ID!");
+		return false;
+	}
+
+	/*
+	There is no need to check if the employeeId entered is an integer.
+	This is because the form entry has type="number", which means this
+	function won't even be called if the entered value isn't an integer.
+	
+	This function also doesn't need to check for ID size because the
+	input's "max" attribute takes care of that.
+	*/
+
+	// Validate password
+	let password = document.forms["signIn"]["password"].value;
+
+	if (password.length == 0)
+	{
+		alert("Please enter your password!");
+		return false;
+	}
+
 	return true;
 }
