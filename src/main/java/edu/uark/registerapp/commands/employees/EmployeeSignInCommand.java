49770558df3ae263d113e49ec22f6f427da752df
nathan fredricks <njfredr@uark.edu>
2021-03-02 20:39:02 -0600

    got rid of errors added setter methods

--- src/main/java/edu/uark/registerapp/commands/employees/EmployeeSignInCommand.java
+++ src/main/java/edu/uark/registerapp/commands/employees/EmployeeSignInCommand.java
@@ -1,108 +1,117 @@
 package edu.uark.registerapp.commands.employees;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.Arrays;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
 
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
+import edu.uark.registerapp.models.api.EmployeeSignIn;
+import edu.uark.registerapp.models.repositories.ActiveUserRepository;
+import edu.uark.registerapp.models.repositories.EmployeeRepository;
 import edu.uark.registerapp.commands.ResultCommandInterface;
 import edu.uark.registerapp.commands.employees.helpers.EmployeeHelper;
 import edu.uark.registerapp.commands.exceptions.UnauthorizedException;
 import edu.uark.registerapp.commands.exceptions.UnprocessableEntityException;
 import edu.uark.registerapp.models.api.Employee;
-import edu.uark.registerapp.models.api.EmployeeSignIn;
 import edu.uark.registerapp.models.entities.ActiveUserEntity;
 import edu.uark.registerapp.models.entities.EmployeeEntity;
-import edu.uark.registerapp.models.repositories.ActiveUserRepository;
-import edu.uark.registerapp.models.repositories.EmployeeRepository;
 
 @Service
-public class EmployeeSignInCommand implements ResultCommandInterface<Employee> {
-	@Override
+public class EmployeeSignInCommand implements ResultCommandInterface<Employee>{
+
 	public Employee execute() {
 		this.validateProperties();
-
-		return new Employee(this.SignInEmployee());
+		return new Employee(this.SignInActiveUser());
 	}
 
-	// Helper methods
 	private void validateProperties() {
+		//ID should not be blank. Should be a number
 		if (StringUtils.isBlank(this.employeeSignIn.getEmployeeId())) {
 			throw new UnprocessableEntityException("employee ID");
 		}
+
+		//ID should only be a number
 		try {
 			Integer.parseInt(this.employeeSignIn.getEmployeeId());
-		} catch (final NumberFormatException e) {
+		} catch (NumberFormatException e) {
 			throw new UnprocessableEntityException("employee ID");
 		}
-		if (StringUtils.isBlank(this.employeeSignIn.getPassword())) {
+
+		if (StringUtils.isBlank(this.employeeSignIn.getPassword())){
 			throw new UnprocessableEntityException("password");
 		}
 	}
 
-	@Transactional
-	private EmployeeEntity SignInEmployee() {
-		final Optional<EmployeeEntity> employeeEntity =
-			this.employeeRepository.findByEmployeeId(
-				Integer.parseInt(this.employeeSignIn.getEmployeeId()));
-
-		if (!employeeEntity.isPresent()
-			|| !Arrays.equals(
-				employeeEntity.get().getPassword(),
-				EmployeeHelper.hashPassword(this.employeeSignIn.getPassword()))
-		) {
-
+	private Optional<EmployeeEntity> queryEmployeeUsingID() {
+		int employeeID = Integer.parseInt(this.getEmployeeId());
+		String password = this.getPassword();
+		Optional<EmployeeEntity> employeeEntity = this.employeeRepository.findByEmployeeId(employeeID);
+		if (!employeeEntity.isPresent() || !Arrays.equals(employeeEntity.get().getPassword(), EmployeeHelper.hashPassword(password)))
+		{
 			throw new UnauthorizedException();
 		}
 
-		final Optional<ActiveUserEntity> activeUserEntity =
-			this.activeUserRepository
-				.findByEmployeeId(employeeEntity.get().getId());
+		return employeeEntity;
+	}
 
-		if (!activeUserEntity.isPresent()) {
+	@Transactional
+	private EmployeeEntity SignInActiveUser(){
+		Optional<EmployeeEntity> employeeEntity = queryEmployeeUsingID();
+
+		//query the activeuser table for a record with the employee ID
+		Optional<ActiveUserEntity> activeUserEntity = 
+			this.activeUserRepository.findByEmployeeId(employeeEntity.get().getId());
+
+			//create a new active user in the record in the database if there isn't one already
+		if(!activeUserEntity.isPresent()) {
 			this.activeUserRepository.save(
-					(new ActiveUserEntity())
-						.setSessionKey(this.sessionId)
-						.setEmployeeId(employeeEntity.get().getId())
-						.setClassification(
-							employeeEntity.get().getClassification())
-						.setName(
-							employeeEntity.get().getFirstName()
-								.concat(" ")
-								.concat(employeeEntity.get().getLastName())));
-		} else {
-			this.activeUserRepository.save(
-				activeUserEntity.get().setSessionKey(this.sessionId));
+				(new ActiveUserEntity())
+					.setSessionKey(this.getSessionId())
+					.setEmployeeId(employeeEntity.get().getId())
+					.setClassification(employeeEntity.get().getClassification())
+					.setName(employeeEntity.get().getFirstName().concat(" ").concat(employeeEntity.get().getLastName()))
+				);
 		}
-
+		else {
+			this.activeUserRepository.save(
+				activeUserEntity.get().setSessionKey(this.getSessionId())
+			);
+		}
 		return employeeEntity.get();
 	}
 
-	// Properties
+
+	//properties
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	@Autowired
+	private ActiveUserRepository activeUserRepository;
+
 	private EmployeeSignIn employeeSignIn;
-	public EmployeeSignIn getEmployeeSignIn() {
-		return this.employeeSignIn;
-	}
-	public EmployeeSignInCommand setEmployeeSignIn(final EmployeeSignIn employeeSignIn) {
-		this.employeeSignIn = employeeSignIn;
-		return this;
+	public String getEmployeeId(){
+		return this.employeeSignIn.getEmployeeId();
 	}
 
-	private String sessionId;
+	public String getPassword(){
+		return this.employeeSignIn.getPassword();
+	}
+	private String sessionId; //session key
 	public String getSessionId() {
 		return this.sessionId;
 	}
+
 	public EmployeeSignInCommand setSessionId(final String sessionId) {
 		this.sessionId = sessionId;
 		return this;
 	}
 
-	@Autowired
-	private EmployeeRepository employeeRepository;
-	@Autowired
-	private ActiveUserRepository activeUserRepository;
+	public EmployeeSignInCommand setEmployeeSignIn(final EmployeeSignIn employeeSignIn) {
+		this.employeeSignIn = employeeSignIn;
+		return this;
+	}
 }
