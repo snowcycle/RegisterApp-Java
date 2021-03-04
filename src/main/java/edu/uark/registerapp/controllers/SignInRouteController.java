74b7a8d1d6282ed76319e59d94ad5934aa4c67e1
fxnggg <74802761+fxnggg@users.noreply.github.com>
2021-03-03 23:11:15 -0600

    Add EmployeeSignIn object parameter

--- src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
@@ -2,13 +2,19 @@ package edu.uark.registerapp.controllers;
 
 import javax.servlet.http.HttpServletRequest;
 
+import java.util.Map;
+
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import edu.uark.registerapp.controllers.enums.ViewNames;
+
+import edu.uark.registerapp.models.api.EmployeeSignIn;
+
 
 @Controller
 @RequestMapping(value = "/")
@@ -23,12 +29,13 @@ public class SignInRouteController extends BaseRouteController
 	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
 	public ModelAndView performSignIn(
 		// TODO: Define an object that will represent the sign in request and add it as a parameter here
-		HttpServletRequest request)
+		EmployeeSignIn signIn, HttpServletRequest request)
 	{
 
 		// TODO: Use the credentials provided in the request body
 		//  and the "id" property of the (HttpServletRequest)request.getSession() variable
 		//  to sign in the user
+
 
 		return new ModelAndView(
 			REDIRECT_PREPEND.concat(
