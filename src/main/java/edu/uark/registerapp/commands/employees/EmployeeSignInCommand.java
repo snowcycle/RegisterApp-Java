aa2bc340538a742642a2c20566b363e65ed98d4f
Jackson Bullard <jsbullar@uark.edu>
2021-03-04 13:40:10 -0600

    Completed the EmployeeSignInCommand.java class.

--- src/main/java/edu/uark/registerapp/commands/employees/EmployeeSignInCommand.java
+++ src/main/java/edu/uark/registerapp/commands/employees/EmployeeSignInCommand.java
@@ -1,6 +1,17 @@
 package edu.uark.registerapp.commands.employees;
 
+import java.util.Optional;
+import java.util.Arrays; 
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.transaction.Transactional;
+
 import edu.uark.registerapp.models.api.EmployeeSignIn;
+import edu.uark.registerapp.models.entities.ActiveUserEntity;
+import edu.uark.registerapp.models.entities.EmployeeEntity;
+import edu.uark.registerapp.models.repositories.ActiveUserRepository;
 import edu.uark.registerapp.models.repositories.EmployeeRepository;
 
 
@@ -8,17 +19,73 @@ public class EmployeeSignInCommand {
 
 	private EmployeeSignIn employeeSignIn;
 	private String sessionKey;
-	
-	// TO DO: Validate employeeSignIn's employeeId and password
-	boolean validateSignIn()
+
+	EmployeeSignInCommand(EmployeeSignIn employeeSignIn, String sessionKey)
 	{
-		if(employeeSignIn.getId().equals(""))
+		this.employeeSignIn = employeeSignIn;
+		this.sessionKey = sessionKey;
+	}
+
+	@Transactional
+	boolean execute()
+	{
+		if (!validateSignIn())	// Credentials are invalid
 			return false;
 
-		String idNum = employeeSignIn.getId();
-		for(int i = 0; i < idNum.length(); i++)
+		Optional<EmployeeEntity> employee =		// Finds employee by employee ID
+			employeeRepository.findByEmployeeId(
+				Integer.parseInt(employeeSignIn.getId()));
+		
+		if (employee.isEmpty())		// Employee does not exist
+			return false;
+
+		if (!Arrays.equals(			// Passwords do NOT match
+			employeeSignIn.getPassword().getBytes(),
+			employee.get().getPassword()))
 		{
-			if(!(idNum.charAt(i) >= 0 && idNum.charAt(i) <= 9))
+			return false;
+		}
+
+		// NOTE: At this point, we have a valid sign in
+
+		Optional<ActiveUserEntity> activeUser =		// Finds active user by UUID
+			activeUserRepository.findByEmployeeId(employee.get().getId());
+
+		if (activeUser.isPresent())		// Updates existing active user's session key
+		{
+			ActiveUserEntity user = activeUser.get().setSessionKey(sessionKey);
+			activeUserRepository.save(user);
+		}
+		else	// Creates new active user
+		{
+			ActiveUserEntity user = new ActiveUserEntity();
+
+			user.setClassification(employee.get().getClassification());
+
+			// Create full name then set active user name
+			String employeeName = employee.get().getFirstName();
+			employeeName.concat(" ").concat(employee.get().getLastName());
+			user.setName(employeeName);
+
+			user.setEmployeeId(employee.get().getId());
+			user.setSessionKey(sessionKey);
+
+			activeUserRepository.save(user);
+		}
+
+		return true;
+	}
+	
+	private boolean validateSignIn()
+	{
+		String employeeId = employeeSignIn.getId();
+
+		if (StringUtils.isBlank(employeeId) || employeeId.length() > 5)
+			return false;
+
+		for (int i = 0; i < employeeId.length(); i++)
+		{
+			if(!(employeeId.charAt(i) >= 0 && employeeId.charAt(i) <= 9))
 				return false;
 		}
 
@@ -26,11 +93,11 @@ public class EmployeeSignInCommand {
 			return false;
 		
 		return true; 
-		
 	}
-	// TO DO: Query employee by id and verify matching passwords
-	/*if(validateSignIn())
-	{
-		EmployeeRepository.queryByEmployeeId() 
-	}*/
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@Autowired
+	private ActiveUserRepository activeUserRepository;
 }
--- src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
@@ -28,8 +28,8 @@ public class SignInRouteController extends BaseRouteController
 
 	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
 	public ModelAndView performSignIn(
-		// TODO: Define an object that will represent the sign in request and add it as a parameter here
-		EmployeeSignIn signIn, HttpServletRequest request)
+		EmployeeSignIn signIn,
+		HttpServletRequest request)
 	{
 
 		// TODO: Use the credentials provided in the request body
