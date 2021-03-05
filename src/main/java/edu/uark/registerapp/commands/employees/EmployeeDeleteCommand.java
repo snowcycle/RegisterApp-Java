455d6ad4c6afa6dffe97d4176a35cfe62d0eca70
uarkNathanSecrest <77640500+uarkNathanSecrest@users.noreply.github.com>
2021-03-04 21:32:14 -0600

    Create EmployeeDeleteCommand.java

---
+++ src/main/java/edu/uark/registerapp/commands/employees/EmployeeDeleteCommand.java
@@ -0,0 +1,42 @@
+package edu.uark.registerapp.commands.employees;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.uark.registerapp.commands.VoidCommandInterface;
+import edu.uark.registerapp.commands.exceptions.NotFoundException;
+import edu.uark.registerapp.models.entities.EmployeeEntity;
+import edu.uark.registerapp.models.repositories.EmployeeRepository;
+
+@Service
+public class EmployeeDeleteCommand implements VoidCommandInterface {
+	@Transactional
+	@Override
+	public void execute() {
+		final Optional<EmployeeEntity> employeeEntity =
+			this.employeeRepository.findById(this.employeeId);
+
+		if (!employeeEntity.isPresent()) { // No record with the associated record ID exists in the database.
+			throw new NotFoundException("Product");
+		}
+
+		this.employeeRepository.delete(employeeEntity.get());
+	}
+
+	// Properties
+	private UUID employeeId;
+	public UUID getEmployeeId() {
+		return this.employeeId;
+	}
+	public EmployeeDeleteCommand setEmployeeId(final UUID productId) {
+		this.employeeId = productId;
+		return this;
+	}
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+}
