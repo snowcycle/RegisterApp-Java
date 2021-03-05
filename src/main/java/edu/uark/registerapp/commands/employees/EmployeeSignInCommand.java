a70343ff40c8fb639e4fc19d66014f7661b52e71
Chase <chasegarrett@tutanota.com>
2021-03-04 19:02:09 -0600

    added dependencies

--- src/main/java/edu/uark/registerapp/commands/employees/EmployeeSignInCommand.java
+++ src/main/java/edu/uark/registerapp/commands/employees/EmployeeSignInCommand.java
@@ -1,6 +1,24 @@
 package edu.uark.registerapp.commands.employees;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import edu.uark.registerapp.commands.ResultCommandInterface;
+import edu.uark.registerapp.commands.employees.helpers.EmployeeHelper;
+import edu.uark.registerapp.commands.exceptions.UnauthorizedException;
+import edu.uark.registerapp.commands.exceptions.UnprocessableEntityException;
+import edu.uark.registerapp.models.api.Employee;
+import edu.uark.registerapp.models.api.EmployeeSignIn;
+import edu.uark.registerapp.models.entities.ActiveUserEntity;
+import edu.uark.registerapp.models.entities.EmployeeEntity;
+import edu.uark.registerapp.models.repositories.ActiveUesrRepository;
+import edu.uark.registerapp.models.repositories.EmployeeRepository;
+
+import java.util.Arrays;
+import java.util.Optional;
+import javax.transaction.Transactional;
+
+@Service
 public class EmployeeSignInCommand{
 }
